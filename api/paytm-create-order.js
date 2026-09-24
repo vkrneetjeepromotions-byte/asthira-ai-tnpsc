40d9b331acc6f3066f0385578cf995eb88823212
vkrneetjeepromotions-byte <vkrneetjeepromotions@gmail.com>
2026-09-24 23:06:44 +1000

    Update paytm-create-order.js

--- api/paytm-create-order.js
+++ api/paytm-create-order.js
@@ -127,7 +127,7 @@ export default async function handler(req, res) {
     console.error("Paytm create order error:", error);
 
     return res.status(500).json({
-      error: "Internal server error while creating Paytm transaction.",
+      error: error.message || "Internal server error while creating Paytm transaction.",
     });
   }
 }
